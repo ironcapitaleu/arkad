cf28fd6a84c0178e260fef450734a0d9b7d54273
Demir Catovic <d.catovic9@gmail.com>
2024-09-05 21:02:16 +0200

    feat: implement first Retrieval structs

--- sec/src/sec_state_machine/ingestion/retrieval/mod.rs
+++ sec/src/sec_state_machine/ingestion/retrieval/mod.rs
@@ -1,1 +1,13 @@
+use state_maschine::prelude::*;
 
+pub mod retrieval_context;
+pub mod retrieval_data;
+
+pub use retrieval_context::RetrievalContext;
+pub use retrieval_data::RetievalData;
+
+pub struct Retrieval {
+    input: RetievalData,
+    output: Option<RetievalData>,
+    context: RetrievalContext,
+}
