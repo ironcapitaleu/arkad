51d56dfc80b237549f44f8f38c1923f8cccbbb92
Demir Catovic <d.catovic9@gmail.com>
2024-09-08 01:26:40 +0200

    test: add initial unit test to retrieval_context config

--- sec/src/sec_state_machine/ingestion/retrieval/retrieval_context/config.rs
+++ sec/src/sec_state_machine/ingestion/retrieval/retrieval_context/config.rs
@@ -13,3 +13,16 @@ pub fn get_sec_user_client() -> Result<Client, Error> {
 pub fn get_sec_user_agent() -> impl ToString {
     USER_AGENT.to_string()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_return_default_email_when_calling_get_sec_user_agent() {
+        let expected_user_agent = "Demir Catovic d.catovic9@gmail.com";
+        let result = get_sec_user_agent().to_string();
+
+        assert_eq!(result, expected_user_agent);
+    }
+}
