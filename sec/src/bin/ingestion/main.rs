f7b0140ccc5b74debf7cb3cad9ab7e266f5a559a
Demir Catovic <d.catovic9@gmail.com>
2024-09-08 22:45:33 +0200

    feat: add tokio dep and finish first successful retrieval of BRK stock by CIK

--- sec/src/bin/ingestion/main.rs
+++ sec/src/bin/ingestion/main.rs
@@ -1,13 +1,38 @@
 use sec::sec_state_machine::ingestion::retrieval::Retrieval;
 use state_maschine::prelude::*;
+use std::error::Error;
+// fn main() {
+//     let retrieval_state = Retrieval::default();
 
-fn main() {
+//     let _state_data = retrieval_state.get_input_data();
+//     let context = retrieval_state.get_context_data();
+    
+//     println!("{retrieval_state}");
+
+//     println!("CIK to retrieve: {}", context.cik());
+//     let retrieval_result = retrieval_state.compute_output_new()
+//     .await?;
+
+// }
+
+
+// Make the main function async using the #[tokio::main] macro
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let retrieval_state = Retrieval::default();
 
-    let state_data = retrieval_state.get_input_data();
+    let _state_data = retrieval_state.get_input_data();
     let context = retrieval_state.get_context_data();
-
-    println!("Context:\n{context}");
-    println!("State data:\n{state_data}");
+    
     println!("{retrieval_state}");
+
+    println!("CIK to retrieve: {}", context.cik());
+
+    // Call the async function and await the result
+    let retrieval_result = retrieval_state.compute_output_new().await?;
+
+    // You can handle the result here
+    println!("Retrieval result: {:?}", retrieval_result);
+
+    Ok(())
 }
