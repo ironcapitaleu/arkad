fc862d9fff37a1d48183688c7924cd7963ea597a
DeCa09 <59141642+DeCa09@users.noreply.github.com>
2024-09-08 17:34:51 +0200

    Fix/ide unresolved import error (#7)
    * fix: unresolved import on workspace import from state_maschine crate in sec crate
    
    * fix: unresolved import on workspace import from state_maschine crate in sec crate
    
    * fix: clippy lint

--- state_machine/src/prelude.rs
+++ state_machine/src/prelude.rs
@@ -1,4 +1,5 @@
-//! The `state_maschine` prelude imports the various traits defined in this crate.
+//! The `state_maschine` imports the various traits defined in this crate.
+//!
 //! The intention is that one can include `use state_maschine::prelude::*` and
 //! have easy access to the various traits and methods you will need.
 
