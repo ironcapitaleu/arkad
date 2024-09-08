9de1b7a618bc6afd4ff643a3371ba7a7026eff3a
Demir Catovic <d.catovic9@gmail.com>
2024-09-08 22:11:16 +0200

    refactor: debug print output

--- sec/src/bin/ingestion.rs
+++ sec/src/bin/ingestion.rs
@@ -4,10 +4,10 @@ use state_maschine::prelude::*;
 fn main() {
     let retrieval_state = Retrieval::default();
 
-    // let state_data = retrieval_state.get_input_data();
-    // let context = retrieval_state.get_context_data();
+    let state_data = retrieval_state.get_input_data();
+    let context = retrieval_state.get_context_data();
 
-    // println!("Context:\n{context}");
-    // println!("State data:\n{state_data}");
+    println!("Context:\n{context}");
+    println!("State data:\n{state_data}");
     println!("{retrieval_state}");
 }
