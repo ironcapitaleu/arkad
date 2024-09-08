74e156ce35b6a810959004274ede7032ee64da93
Demir Catovic <d.catovic9@gmail.com>
2024-09-08 22:51:10 +0200

    feat: add some info around how the Retrieval State looks before and after querying the SEC API

--- sec/src/bin/ingestion/main.rs
+++ sec/src/bin/ingestion/main.rs
@@ -23,12 +23,16 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let _state_data = retrieval_state.get_input_data();
     let context = retrieval_state.get_context_data();
 
+    println!("Initial Retrieval state:");
     println!("{retrieval_state}");
 
     println!("CIK to retrieve: {}", context.cik());
 
     // Call the async function and await the result
     retrieval_state.compute_output_new().await?;
+ 
+    println!("\nRetrieval state after quering SEC API with CIK:");
+    println!("{retrieval_state}");
 
     Ok(())
 }
