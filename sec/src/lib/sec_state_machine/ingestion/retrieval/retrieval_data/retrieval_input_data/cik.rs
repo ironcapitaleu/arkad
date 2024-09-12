facc704414198c8fb916f12479ca0f934f573c7d
Demir Catovic <d.catovic9@gmail.com>
2024-09-12 17:06:30 +0200

    style: code formatting

--- sec/src/lib/sec_state_machine/ingestion/retrieval/retrieval_data/retrieval_input_data/cik.rs
+++ sec/src/lib/sec_state_machine/ingestion/retrieval/retrieval_data/retrieval_input_data/cik.rs
@@ -16,21 +16,20 @@ impl CIK {
     /// This function will panic if the input string contains non-numeric characters or if it's longer than 10 digits.
     ///
     /// # Examples
-    /// 
+    ///
     /// ```
     /// use sec::sec_state_machine::ingestion::retrieval::retrieval_data::retrieval_input_data::CIK;
-    /// 
+    ///
     /// let cik = CIK::new("123456789");
     /// assert_eq!(cik.value(), "0123456789");
     /// ```
-
 
     pub fn new(cik: &(impl ToString + ?Sized)) -> Self {
         let mut cik_str = cik.to_string().trim().to_string(); // Trim leading and trailing whitespace
 
         // Check if it contains only digits
         assert!(
-            cik_str.chars().all(|c| c.is_ascii_digit()), 
+            cik_str.chars().all(|c| c.is_ascii_digit()),
             "Invalid CIK: CIK must contain only numeric characters. Got: {}",
             cik.to_string()
         );
@@ -136,7 +135,8 @@ mod tests {
     }
 
     #[test]
-    fn should_trim_whitespace_from_cik_input_str_when_passed_string_with_leading_or_trailing_whitespace() {
+    fn should_trim_whitespace_from_cik_input_str_when_passed_string_with_leading_or_trailing_whitespace(
+    ) {
         let cik_str = "     0123456789     ";
 
         let expected_result = "0123456789";
