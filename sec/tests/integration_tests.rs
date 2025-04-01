4c9bdc890e789a7d7b9162b2ff038ec9afd0056f
Demir Catovic <d.catovic9@gmail.com>
2025-04-01 18:34:15 +0200

    tests: refactor test names for better understandability

--- sec/tests/integration_tests.rs
+++ sec/tests/integration_tests.rs
@@ -2,7 +2,7 @@ use sec::sec_state_machine::State;
 use sec::sec_state_machine::ingestion::retrieval::Retrieval;
 
 #[test]
-fn should_return_true_when_state_has_computed_the_output() {
+fn should_return_true_when_retrieval_state_has_computed_the_output() {
     let mut retrieval_state = Retrieval::default();
 
     let expected_result = true;
@@ -14,7 +14,7 @@ fn should_return_true_when_state_has_computed_the_output() {
 }
 
 #[test]
-fn should_return_true_when_reference_state_has_computed_the_output() {
+fn should_return_true_when_reference_of_retrieval_state_has_computed_the_output() {
     let ref_to_retrieval_state = &mut Retrieval::default();
 
     let expected_result = true;
