4dcec4dce269942cfde946db984db344dceaeb57
ElecDashi <elecdashi@gmail.com>
2025-04-01 15:01:55 +0200

    fix: move integration tests to correct directory

---
+++ sec/tests/integration_tests.rs
@@ -0,0 +1,26 @@
+use sec::sec_state_machine::ingestion::retrieval::Retrieval;
+use sec::sec_state_machine::State;
+
+#[test]
+fn should_return_true_when_state_has_computed_the_output() {
+    let mut retrieval_state = Retrieval::default();
+
+    let expected_result = true;
+
+    retrieval_state.compute_output_data();
+    let result = retrieval_state.has_output_data_been_computed();
+
+    assert_eq!(result, expected_result);
+}
+
+#[test]
+fn should_return_true_when_reference_state_has_computed_the_output() {
+    let ref_to_retrieval_state = &mut Retrieval::default();
+
+    let expected_result = true;
+
+    ref_to_retrieval_state.compute_output_data();
+    let result = ref_to_retrieval_state.has_output_data_been_computed();
+
+    assert_eq!(result, expected_result);
+}
