446cd24aa33f5e9b6c5010bb6568ea0714e5cc5d
ElecDashi <elecdashi@gmail.com>
2025-05-12 12:42:55 +0200

    feat: simplify SecContextData retries

--- sec/src/lib/sec_state_machine/sec_context_data.rs
+++ sec/src/lib/sec_state_machine/sec_context_data.rs
@@ -1,8 +1,9 @@
 use state_maschine::prelude::*;
 
 pub trait SecContextData: ContextData {
-    fn can_retry(&self) -> bool;
-    fn get_max_retries(&self) -> u32;
-    fn get_current_retry(&self) -> u32;
-    fn has_retries_left(&self) -> bool;
+    fn can_retry(&self) -> bool {
+        self.get_retries() > 0
+    }
+    
+    fn get_retries(&self) -> u32;
 }
