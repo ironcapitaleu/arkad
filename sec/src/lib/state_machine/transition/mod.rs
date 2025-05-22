85be3cb8f1831c7c788b0e1cc2533e46f499e32a
Demir Catovic <d.catovic9@gmail.com>
2025-05-22 10:09:42 +0200

    refactor: rename `SecTransition` trait to `Transition`

--- sec/src/lib/state_machine/transition/mod.rs
+++ sec/src/lib/state_machine/transition/mod.rs
@@ -1,7 +1,7 @@
 use crate::state_machine::state::State;
-use state_maschine::prelude::*;
+use state_maschine::prelude::Transition as SMTransition;
 
-pub trait SecTransition<T, U>: Transition<T, U>
+pub trait Transition<T, U>: SMTransition<T, U>
 where
     T: State,
     U: State,
