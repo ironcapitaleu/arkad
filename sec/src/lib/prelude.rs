f0f66bed73ecbceef7c32475eb0f8b98b4494fd5
Demir Catovic <d.catovic9@gmail.com>
2025-05-22 09:03:59 +0200

    refactor: add prelude to include imports to the important `sec_state_machine`traits

---
+++ sec/src/lib/prelude.rs
@@ -0,0 +1,11 @@
+//! The `sec` imports various traits defined in this crate.
+//!
+//! The intention is that one can include `use sec::prelude::*` and
+//! have easy access to the various traits and methods you will need.
+
+pub use crate::sec_state_machine::SecStateMachine;
+pub use crate::sec_state_machine::sec_state::SecContextData;
+pub use crate::sec_state_machine::sec_state::SecState;
+pub use crate::sec_state_machine::sec_state::SecStateData;
+pub use crate::sec_state_machine::sec_super_state::SecSuperState;
+pub use crate::sec_state_machine::sec_transition::SecTransition;
