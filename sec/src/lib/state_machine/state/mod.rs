2ab4adb4ce8ea22dd57a978875cb4236d8da1f05
Demir Catovic <d.catovic9@gmail.com>
2025-05-22 09:54:19 +0200

    refactor: rename `sec_state__data` module to `state_data`

--- sec/src/lib/state_machine/state/mod.rs
+++ sec/src/lib/state_machine/state/mod.rs
@@ -3,10 +3,10 @@ use state_maschine::prelude::State as SMState;
 use super::sec_error::SecError;
 
 pub mod context_data;
-pub mod sec_state_data;
+pub mod state_data;
 
 pub use context_data::ContextData;
-pub use sec_state_data::SecStateData;
+pub use state_data::SecStateData;
 
 pub trait State: SMState {
     /// Computes the output data for the SEC state.
