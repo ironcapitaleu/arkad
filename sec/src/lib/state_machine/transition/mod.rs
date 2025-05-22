16d4424f42195d36dd121371e2bf437919fd4602
Demir Catovic <d.catovic9@gmail.com>
2025-05-22 10:09:59 +0200

    refactor: re-order imports

--- sec/src/lib/state_machine/transition/mod.rs
+++ sec/src/lib/state_machine/transition/mod.rs
@@ -1,5 +1,6 @@
-use crate::state_machine::state::State;
 use state_maschine::prelude::Transition as SMTransition;
+
+use crate::state_machine::state::State;
 
 pub trait Transition<T, U>: SMTransition<T, U>
 where
