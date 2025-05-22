b9d0b4b49885afe72d7334f49eea9686995a2896
Demir Catovic <d.catovic9@gmail.com>
2025-05-22 11:29:43 +0200

    feat: implement error trait for `StateMachine`

--- sec/src/lib/error/state_machine/mod.rs
+++ sec/src/lib/error/state_machine/mod.rs
@@ -5,6 +5,7 @@ pub use state::State;
 pub use transition::Transition;
 
 #[non_exhaustive]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum StateMachine {
     /// Invalid configuration of the state machine.
     InvalidConfiguration,
@@ -15,3 +16,22 @@ pub enum StateMachine {
     /// Transtion related error.
     Transition,
 }
+
+impl std::fmt::Display for StateMachine {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::State => {
+                write!(f, "Problem occured during internal state operations.")
+            },
+            Self::Transition => {
+                write!(f, "Problem occured during state transition.")
+            },
+            Self::InvalidConfiguration => {
+                write!(f, "Invalid configuration of the state machine.")
+            },
+        }
+    }
+}
+
+impl std::error::Error for StateMachine {}
+
