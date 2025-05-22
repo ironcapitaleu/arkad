adfcab9cf2fa327d75070cac575643e676c46198
Demir Catovic <d.catovic9@gmail.com>
2025-05-22 11:32:27 +0200

    feat: implement `Error` trait for `State` and `Transition`

--- sec/src/lib/error/state_machine/state/mod.rs
+++ sec/src/lib/error/state_machine/state/mod.rs
@@ -1,2 +1,17 @@
 #[non_exhaustive]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum State {}
+
+
+impl std::fmt::Display for State {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            _ => {
+                write!(f, "Problem occured during internal state operations.")
+            },
+        }
+    }
+}
+
+impl std::error::Error for State {}
+
--- sec/src/lib/error/state_machine/transition/mod.rs
+++ sec/src/lib/error/state_machine/transition/mod.rs
@@ -1,2 +1,16 @@
 #[non_exhaustive]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Transition {}
+
+
+impl std::fmt::Display for Transition {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            _ => {
+                write!(f, "Problem occured during transition operations.")
+            },
+        }
+    }
+}
+
+impl std::error::Error for Transition {}
