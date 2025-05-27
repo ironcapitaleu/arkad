04fc25d6553540c227f24df1981653159742927b
Demir Catovic <d.catovic9@gmail.com>
2025-05-27 16:40:32 +0200

    refactor: use separate module for defining constants

---
+++ sec/src/lib/shared/cik/constants.rs
@@ -0,0 +1,5 @@
+/// The required length for a valid CIK (Central Index Key).
+///
+/// This constant defines the exact number of digits a CIK must have to be considered valid
+/// according to SEC requirements.
+pub const CIK_LENGTH: usize = 10;
