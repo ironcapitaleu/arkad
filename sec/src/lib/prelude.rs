8efb2af00714ed8956446e66ee75dab582f05f0b
Demir Catovic <d.catovic9@gmail.com>
2025-05-28 11:26:02 +0200

    docs: add docstrings for prelude

--- sec/src/lib/prelude.rs
+++ sec/src/lib/prelude.rs
@@ -1,7 +1,23 @@
-//! The `sec` imports various traits defined in this crate.
+//! # SEC Prelude Module
 //!
-//! The intention is that one can include `use sec::prelude::*` and
-//! have easy access to the various traits and methods you will need.
+//! This module provides convenient re-exports of the core SEC state machine traits for downstream crates.
+//! By importing `sec::prelude::*`, users gain easy access to the essential building blocks for defining,
+//! composing, and interacting with SEC state machines, states, context, and transitions.
+//!
+//! ## Re-exports
+//! - [`SecStateMachine`](crate::traits::state_machine::SecStateMachine): Trait for SEC-specific state machines.
+//! - [`State`](crate::traits::state_machine::state::State): Trait for defining individual states.
+//! - [`StateData`](crate::traits::state_machine::state::StateData): Trait for state data management.
+//! - [`ContextData`](crate::traits::state_machine::state::ContextData): Trait for context data management.
+//! - [`SuperState`](crate::traits::state_machine::super_state::SuperState): Trait for hierarchical state machines.
+//! - [`Transition`](crate::traits::state_machine::transition::Transition): Trait for defining state transitions.
+//!
+//! ## Usage
+//!
+//! ```rust
+//! use sec::prelude::*;
+//! // Now you can use State, StateData, ContextData, etc. directly.
+//! ```
 
 pub use crate::traits::state_machine::SecStateMachine;
 pub use crate::traits::state_machine::state::ContextData;
