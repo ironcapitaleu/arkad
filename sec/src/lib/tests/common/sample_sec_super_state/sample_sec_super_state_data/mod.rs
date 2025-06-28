8697947be5ee9c2cfc37ef3aa5ffa4fd95db9c45
ElecDashi <elecdashi@gmail.com>
2025-06-29 00:51:21 +0200

    feat: add SampleSecSuperState fixture

---
+++ sec/src/lib/tests/common/sample_sec_super_state/sample_sec_super_state_data/mod.rs
@@ -0,0 +1,22 @@
+use crate::error::State as StateError;
+use crate::traits::state_machine::state::StateData;
+use state_maschine::prelude::StateData as SMStateData;
+
+#[derive(Debug, Clone, Default, PartialEq, PartialOrd, Hash, Eq, Ord)]
+pub struct SampleSecSuperStateData;
+
+impl StateData for SampleSecSuperStateData {
+    fn update_state(&mut self, _updates: Self::UpdateType) -> Result<(), StateError> {
+        Ok(())
+    }
+}
+
+impl SMStateData for SampleSecSuperStateData {
+    type UpdateType = ();
+
+    fn get_state(&self) -> &Self {
+        self
+    }
+
+    fn update_state(&mut self, _updates: Self::UpdateType) {}
+}
