6327ba99142d1438e8d6bd079fd4d4a007c8bce4
ElecDashi <53495196+ElecDashi@users.noreply.github.com>
2025-06-29 13:20:23 +0200

    Feat/extract superstate (#29)
    * refactor: fix typo
    
    * feat: initial extract superstate
    
    * feat: add extract superstate in main
    
    * chore: formatting and clippy fixes
    
    * chore: remove error debugging code from main
    
    * feat: simplify input for extract superstate
    
    * feat: clippy fixes
    
    * feat: add initial structure for sample sec super state
    
    * chore: formatting
    
    * feat: add SampleSecSuperState fixture
    
    * test: add unit tests for SuperState trait
    
    * docs: update documentation to include the sample super state
    
    * chore: formatting

--- sec/src/bin/extraction/main.rs
+++ sec/src/bin/extraction/main.rs
@@ -1,16 +1,9 @@
+use sec::implementations::states::extract::ExtractSuperState;
 use sec::implementations::states::extract::validate_cik_format::ValidateCikFormat;
 use sec::prelude::*;
 
-use sec::error::{ErrorKind, StateMachine};
-
 #[tokio::main]
 async fn main() {
-    let err = ErrorKind::StateMachine(StateMachine::InvalidConfiguration);
-    println!("Printing error:");
-    println!("=======================================================");
-    println!("{err}");
-    println!("=======================================================");
-
     let mut validate_cik_state = ValidateCikFormat::default();
 
     println!("\n=======================================================");
@@ -25,4 +18,22 @@ async fn main() {
     println!("\n=======================================================");
     println!("Validation state after verifying CIK:");
     println!("{:.500}", validate_cik_state.to_string().as_str());
+
+    println!("\n=======================================================");
+    println!("Initial Extract SuperState:");
+
+    let mut super_state = ExtractSuperState::new("1067983");
+
+    super_state
+        .compute_output_data_async()
+        .await
+        .expect("Hardcoded default CIK should alaways have a valid format.");
+
+    let validated_cik = super_state
+        .get_current_state()
+        .get_output_data()
+        .unwrap()
+        .validated_cik
+        .value();
+    println!("State 1 Output: Validated CIK is {validated_cik}");
 }
--- sec/src/lib/traits/state_machine/super_state/mod.rs
+++ sec/src/lib/traits/state_machine/super_state/mod.rs
@@ -36,3 +36,148 @@ where
     S: State,
 {
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{fmt::Debug, hash::Hash};
+
+    use pretty_assertions::assert_eq;
+    use tokio;
+
+    use crate::{
+        prelude::*,
+        tests::common::{
+            sample_sec_state::SampleSecState,
+            sample_sec_super_state::{
+                SampleSecSuperState, SampleSecSuperStateContext, SampleSecSuperStateData,
+            },
+        },
+    };
+
+    #[test]
+    fn should_return_super_state_name_when_in_sample_super_state() {
+        let super_state = SampleSecSuperState::<SampleSecState>::new();
+
+        let expected_result = "Sample SEC SuperState (Current: Sample SEC State)";
+
+        let result = super_state.get_state_name().to_string();
+
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn should_return_inner_state_name_when_accessing_current_state() {
+        let super_state = SampleSecSuperState::<SampleSecState>::new();
+
+        let expected_result = "Sample SEC State";
+
+        let result = super_state.get_current_state().get_state_name().to_string();
+
+        assert_eq!(result, expected_result);
+    }
+
+    #[tokio::test]
+    async fn should_compute_output_for_inner_state_when_calling_compute_on_super_state() {
+        let mut super_state = SampleSecSuperState::<SampleSecState>::new();
+
+        // The super state's output is not computed, but the inner state's is.
+        assert!(super_state.get_output_data().is_none());
+        assert!(super_state.get_current_state().get_output_data().is_none());
+
+        super_state
+            .compute_output_data_async()
+            .await
+            .expect("Computation should succeed");
+
+        assert!(super_state.get_output_data().is_none()); // Super state output is still None
+        assert!(super_state.get_current_state().get_output_data().is_some()); // Inner state has output
+    }
+
+    #[test]
+    fn should_return_default_context_data_when_in_initial_super_state() {
+        let super_state = SampleSecSuperState::<SampleSecState>::new();
+        let expected_result = &SampleSecSuperStateContext::default();
+        let result = super_state.get_context_data();
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn should_return_default_input_data_when_in_initial_super_state() {
+        let super_state = SampleSecSuperState::<SampleSecState>::new();
+        let expected_result = &SampleSecSuperStateData::default();
+        let result = super_state.get_input_data();
+        assert_eq!(result, expected_result);
+    }
+
+    const fn implements_auto_traits<T: Sized + Send + Sync + Unpin>() {}
+    #[test]
+    const fn should_still_implement_auto_traits_when_implementing_super_state_trait() {
+        implements_auto_traits::<SampleSecSuperState<SampleSecState>>();
+    }
+
+    const fn implements_send<T: Send>() {}
+    #[test]
+    const fn should_implement_send_when_implementing_super_state_trait() {
+        implements_send::<SampleSecSuperState<SampleSecState>>();
+    }
+
+    const fn implements_sync<T: Sync>() {}
+    #[test]
+    const fn should_implement_sync_when_implementing_super_state_trait() {
+        implements_sync::<SampleSecSuperState<SampleSecState>>();
+    }
+
+    const fn implements_sized<T: Sized>() {}
+    #[test]
+    const fn should_be_sized_when_implementing_super_state_trait() {
+        implements_sized::<SampleSecSuperState<SampleSecState>>();
+    }
+
+    const fn implements_hash<T: Hash>() {}
+    #[test]
+    const fn should_implement_hash_when_implementing_super_state_trait() {
+        implements_hash::<SampleSecSuperState<SampleSecState>>();
+    }
+
+    const fn implements_partial_eq<T: PartialEq>() {}
+    #[test]
+    const fn should_implement_partial_eq_when_implementing_super_state_trait() {
+        implements_partial_eq::<SampleSecSuperState<SampleSecState>>();
+    }
+
+    const fn implements_eq<T: Eq>() {}
+    #[test]
+    const fn should_implement_eq_when_implementing_super_state_trait() {
+        implements_eq::<SampleSecSuperState<SampleSecState>>();
+    }
+
+    const fn implements_partial_ord<T: PartialOrd>() {}
+    #[test]
+    const fn should_implement_partial_ord_when_implementing_super_state_trait() {
+        implements_partial_ord::<SampleSecSuperState<SampleSecState>>();
+    }
+
+    const fn implements_ord<T: Ord>() {}
+    #[test]
+    const fn should_implement_ord_when_implementing_super_state_trait() {
+        implements_ord::<SampleSecSuperState<SampleSecState>>();
+    }
+
+    const fn implements_debug<T: Debug>() {}
+    #[test]
+    const fn should_implement_debug_when_implementing_super_state_trait() {
+        implements_debug::<SampleSecSuperState<SampleSecState>>();
+    }
+
+    const fn implements_clone<T: Clone>() {}
+    #[test]
+    const fn should_implement_clone_when_implementing_super_state_trait() {
+        implements_clone::<SampleSecSuperState<SampleSecState>>();
+    }
+
+    const fn implements_unpin<T: Unpin>() {}
+    #[test]
+    const fn should_implement_unpin_when_implementing_super_state_trait() {
+        implements_unpin::<SampleSecSuperState<SampleSecState>>();
+    }
+}
