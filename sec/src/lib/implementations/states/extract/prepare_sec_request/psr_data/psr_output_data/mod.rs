0a0666781535e7e42dc15f76a7ec6609fcc0725a
ElecDashi <elecdashi@gmail.com>
2025-07-08 13:32:01 +0200

    feat: implemented output data for PrepareSecRequest

--- sec/src/lib/implementations/states/extract/prepare_sec_request/psr_data/psr_output_data/mod.rs
+++ sec/src/lib/implementations/states/extract/prepare_sec_request/psr_data/psr_output_data/mod.rs
@@ -1,36 +1,85 @@
-use std::fmt;
+use std::{fmt, hash::Hash};
 
 use state_maschine::prelude::StateData as SMStateData;
 
 use crate::error::State as StateError;
 use crate::traits::state_machine::state::StateData;
 
-#[derive(Debug, Clone, PartialEq, PartialOrd, Hash, Eq, Ord)]
+use reqwest::{Client, Method, Url, Request};
+
+#[derive(Debug)]
 pub struct PrepareSecRequestOutputData {
-    pub output_data: String,
+    pub client: Client,
+    pub request: Request,
+}
+
+impl Clone for PrepareSecRequestOutputData {
+    fn clone(&self) -> Self {
+        Self {
+            client: self.client.clone(),
+            request: self.request.try_clone().expect("Failed to clone RequestBuilder"),
+        }
+    }
+}
+
+impl PartialEq for PrepareSecRequestOutputData {
+    fn eq(&self, other: &Self) -> bool {
+        self.request.url() == other.request.url()    
+    }
+}
+
+impl PartialOrd for PrepareSecRequestOutputData {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.request.url().cmp(other.request.url()))
+    }
+}
+
+impl Ord for PrepareSecRequestOutputData {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.request.url().cmp(other.request.url())
+    }
+}
+
+impl Eq for PrepareSecRequestOutputData {}
+
+impl Hash for PrepareSecRequestOutputData {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.request.url().hash(state);
+    }
 }
 
 impl PrepareSecRequestOutputData {
-    /// Creates a new instance of `PrepareSecRequestOutputData` with the provided data.
+    /// Creates a new instance of `PrepareSecRequestOutputData` with the provided client and request.
     ///
     /// # Errors
     /// Returns a `StateError` if the output data cannot be created from the provided data.
-    pub fn new(data: &(impl ToString + ?Sized)) -> Result<Self, StateError> {
+    pub fn new(client: Client, request: Request) -> Result<Self, StateError> {
         Ok(Self {
-            output_data: data.to_string(),
+            client,
+            request
         })
     }
 
-    /// Returns a reference to the output data string.
+    /// Returns a reference to the client.
     #[must_use]
-    pub const fn output_data(&self) -> &String {
-        &self.output_data
+    pub const fn client(&self) -> &Client {
+        &self.client
+    }
+
+    /// Returns a reference to the request.
+    #[must_use]
+    pub const fn request(&self) -> &Request {
+        &self.request
     }
 }
+
 impl StateData for PrepareSecRequestOutputData {
     fn update_state(&mut self, updates: Self::UpdateType) -> Result<(), StateError> {
-        if let Some(input_data) = updates.output_data {
-            self.output_data = input_data;
+        if let Some(client) = updates.client {
+            self.client = client;
+        }
+        if let Some(request) = updates.request {
+            self.request = request;
         }
         Ok(())
     }
@@ -47,43 +96,53 @@ impl SMStateData for PrepareSecRequestOutputData {
 impl Default for PrepareSecRequestOutputData {
     fn default() -> Self {
         Self {
-            output_data: String::from("Hello World!"),
+            client: Client::new(),
+            request: Request::new(Method::GET, Url::parse("https://httpbin.org/get").expect("Hardcoded URL should always be valid.")),
         }
     }
 }
 
 impl fmt::Display for PrepareSecRequestOutputData {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "\tOutput Data: {}", self.output_data,)
+        write!(f, "\tURL: {}", self.request.url())
     }
 }
 
-#[derive(Debug, Clone, PartialEq, PartialOrd, Hash, Eq, Ord)]
+#[derive(Debug)]
 pub struct PrepareSecRequestOutputDataUpdater {
-    pub output_data: Option<String>,
+    pub client: Option<Client>,
+    pub request: Option<Request>,
 }
 
 pub struct PrepareSecRequestOutputDataUpdaterBuilder {
-    output_data: Option<String>,
+    client: Option<Client>,
+    request: Option<Request>,
 }
 
 impl PrepareSecRequestOutputDataUpdaterBuilder {
     #[must_use]
     pub const fn new() -> Self {
-        Self { output_data: None }
+        Self { client: None, request: None }
     }
 
     #[must_use]
     #[allow(clippy::missing_const_for_fn)]
-    pub fn output_data(mut self, output_data: &(impl ToString + ?Sized)) -> Self {
-        self.output_data = Some(output_data.to_string());
+    pub fn client(mut self, client: Client) -> Self {
+        self.client = Some(client);
+        self
+    }
+    #[must_use]
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn request(mut self, request: Request) -> Self {
+        self.request = Some(request);
         self
     }
 
     #[must_use]
     pub fn build(self) -> PrepareSecRequestOutputDataUpdater {
         PrepareSecRequestOutputDataUpdater {
-            output_data: self.output_data,
+            client: self.client,
+            request: self.request,
         }
     }
 }
