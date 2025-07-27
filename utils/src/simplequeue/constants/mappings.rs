b9a2fb40c02870abbcdbdab062cc19e51d1daad2
Demir Catovic <d.catovic9@gmail.com>
2025-07-27 17:43:54 +0200

    tests: add initial test suite for constant `CONNECTOR_CONFIG_MAP`

--- utils/src/simplequeue/constants/mappings.rs
+++ utils/src/simplequeue/constants/mappings.rs
@@ -37,3 +37,59 @@ pub static CONNECTOR_CONFIG_MAP: LazyLock<HashMap<ConnectorKind, Vec<ChannelConf
         );
         m
     });
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+    
+    #[test]
+    fn should_map_batch_extractor_connector_to_producer_access_for_batch_extractor_queue() {
+        let configs = CONNECTOR_CONFIG_MAP.get(&ConnectorKind::BatchExtractor).expect("`ConnectorKind::BatchExtractor` should always have a config.");
+        
+        let expected_result = 1;
+        let result = configs.len();
+        assert_eq!(result, expected_result);
+        
+        let has_producer_access_to_batch_extractor = configs.iter().any(|config| {
+            matches!(config.channel_type, ChannelType::Producer) &&
+            matches!(config.queue_identifier, QueueIdentifier::BatchExtractor)
+        });
+        assert!(has_producer_access_to_batch_extractor);
+    }
+
+    #[test]
+    fn should_map_batch_transformer_connector_to_consumer_and_producer_access() {
+        let configs = CONNECTOR_CONFIG_MAP.get(&ConnectorKind::BatchTransformer).expect("`ConnectorKind::BatchTransformer` should always have a config.");
+        
+        let expected_result = 2;
+        let result = configs.len();
+        assert_eq!(result, expected_result);
+        
+        let has_consumer_access_to_batch_extractor = configs.iter().any(|config| {
+            matches!(config.channel_type, ChannelType::Consumer) &&
+            matches!(config.queue_identifier, QueueIdentifier::BatchExtractor)
+        });
+        assert!(has_consumer_access_to_batch_extractor);
+        
+        let has_producer_access_to_batch_transformer = configs.iter().any(|config| {
+            matches!(config.channel_type, ChannelType::Producer) &&
+            matches!(config.queue_identifier, QueueIdentifier::BatchTransformer)
+        });
+        assert!(has_producer_access_to_batch_transformer);
+    }
+
+    #[test]
+    fn should_map_batch_loader_connector_to_consumer_access_for_batch_transformer_queue() {
+        let configs = CONNECTOR_CONFIG_MAP.get(&ConnectorKind::BatchLoader).expect("`ConnectorKind::BatchLoader` should always have a config.");
+        
+        let expected_result = 1;
+        let result = configs.len();
+        assert_eq!(result, expected_result);
+        
+        let has_consumer_access_to_batch_transformer = configs.iter().any(|config| {
+            matches!(config.channel_type, ChannelType::Consumer) &&
+            matches!(config.queue_identifier, QueueIdentifier::BatchTransformer)
+        });
+        assert!(has_consumer_access_to_batch_transformer);
+    }
+}
