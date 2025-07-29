bcf6d39239be2811139cd6c8650b891dac37a953
Demir Catovic <d.catovic9@gmail.com>
2025-07-29 15:31:55 +0200

    feat: provide initial mock implementation for `InnerChannel` for  `LapinChannel`

--- utils/src/simplequeue/traits/channel/inner.rs
+++ utils/src/simplequeue/traits/channel/inner.rs
@@ -20,3 +20,12 @@ impl InnerChannel for MockService {
         println!("Mock service for testing.");
     }
 }
+
+// TODO: remove this later and put it in the right place
+use lapin::Channel as LapinChannel;
+// Implementation of InnerChannel for LapinChannel
+impl InnerChannel for LapinChannel {
+    fn serve(&self) {
+        println!("LapinChannel is serving.");
+    }
+}
