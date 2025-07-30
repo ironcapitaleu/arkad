06ba0ebf1e76431511f92044361aafc2bd58f7d2
Demir Catovic <d.catovic9@gmail.com>
2025-07-30 20:02:38 +0200

    feat: define a `Connection` trait

--- utils/src/simplequeue/traits/connection/mod.rs
+++ utils/src/simplequeue/traits/connection/mod.rs
@@ -1,3 +1,9 @@
+use crate::simplequeue::connector::Connector;
+
 pub mod inner;
 
 pub use inner::InnerConnection;
+
+pub trait Connection {
+    fn new<I: InnerConnection>(inner: I, connector: Connector) -> Self;
+}
