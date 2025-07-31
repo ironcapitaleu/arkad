162ef59877108818bc0bc0629f0bce6d243bf497
Demir Catovic <d.catovic9@gmail.com>
2025-07-31 21:01:56 +0200

    refactor: remove old `ProducerChannel` implementaiton

--- utils/src/simplequeue/channel/mod.rs
+++ utils/src/simplequeue/channel/mod.rs
@@ -1,9 +1,7 @@
 pub mod builder;
 pub mod channel_config;
 pub mod consumer_channel;
-pub mod producer_channel;
 
 pub use builder::ChannelBuilder;
 pub use channel_config::{ChannelConfig, ChannelType, QueueIdentifier};
 pub use consumer_channel::ConsumerChannel;
-pub use producer_channel::ProducerChannel;
