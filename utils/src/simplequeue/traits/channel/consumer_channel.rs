a4e32b19c5c46119af91a30a35a4c559e6489072
Demir Catovic <d.catovic9@gmail.com>
2025-07-31 18:19:19 +0200

    feat: add `Channel` trait bound on `ProducerChannel` and `ConsumerChannel`

--- utils/src/simplequeue/traits/channel/consumer_channel.rs
+++ utils/src/simplequeue/traits/channel/consumer_channel.rs
@@ -1,4 +1,6 @@
-pub trait ConsumerChannel {
+use super::Channel;
+
+pub trait ConsumerChannel: Channel {
     type Item: Send + Sync + 'static;
 
     fn consume(&self) -> Result<Self::Item, String>; // TODO: Define a more specific error type
--- utils/src/simplequeue/traits/channel/producer_channel.rs
+++ utils/src/simplequeue/traits/channel/producer_channel.rs
@@ -1,4 +1,6 @@
-pub trait ProducerChannel {
+use super::Channel;
+
+pub trait ProducerChannel: Channel {
     type Item: Send + Sync + 'static;
 
     fn produce(&self, item: Self::Item) -> Result<(), String>; // TODO: Define a more specific error type
