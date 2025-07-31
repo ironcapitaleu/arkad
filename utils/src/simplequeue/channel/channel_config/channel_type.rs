df3c075fb69b703de580e86fce8704a7fd400e55
Demir Catovic <d.catovic9@gmail.com>
2025-07-31 18:20:12 +0200

    docs: modify docstring

--- utils/src/simplequeue/channel/channel_config/channel_type.rs
+++ utils/src/simplequeue/channel/channel_config/channel_type.rs
@@ -1,4 +1,4 @@
-/// The role of a channel in the queue system.
+/// The type of a channel in the queue system.
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum ChannelType {
     Producer,
