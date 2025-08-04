e1f26fa7ca48093f0ca6fd4ce0d1f16ff725fc02
Demir Catovic <d.catovic9@gmail.com>
2025-08-04 03:43:16 +0200

    feat: add methods to create `Channel`s from a `Connection`

--- utils/src/simplequeue/channel/mod.rs
+++ utils/src/simplequeue/channel/mod.rs
@@ -1,6 +1,6 @@
 pub mod builder;
 pub mod channel_config;
 
-pub use crate::simplequeue::implementations::channel::ConsumerChannel;
+pub use crate::simplequeue::implementations::channel::{ConsumerChannel, ProducerChannel};
 pub use builder::ChannelBuilder;
 pub use channel_config::{ChannelConfig, ChannelType, QueueIdentifier};
--- utils/src/simplequeue/traits/connection/mod.rs
+++ utils/src/simplequeue/traits/connection/mod.rs
@@ -1,16 +1,30 @@
+use std::fmt::Debug;
+
 use crate::simplequeue::connector::Connector;
+use crate::simplequeue::traits::Item;
+
+use crate::simplequeue::channel::QueueIdentifier;
+use crate::simplequeue::channel::{ConsumerChannel, ProducerChannel};
 
 pub mod inner;
 
 pub use inner::InnerConnection;
 
-pub trait Connection {
-    fn new<I: InnerConnection>(inner: I, connector: Connector) -> Self;
-}
-
-pub trait Channel {
+pub trait Connection: Send + Sync + 'static + Debug + Clone {
     type Inner: InnerConnection;
+
+    fn new<I: InnerConnection>(inner: I, connector: Connector) -> Self;
 
     fn inner(&self) -> &Self::Inner;
     fn connector(&self) -> &Connector;
+
+    fn create_producer_channel<T: Item>(
+        &self,
+        queue_identifier: QueueIdentifier,
+    ) -> Result<ProducerChannel<Self::Inner, T>, String>;
+
+    fn create_consumer_channel<T: Item>(
+        &self,
+        queue_identifier: QueueIdentifier,
+    ) -> Result<ConsumerChannel<Self::Inner, T>, String>;
 }
