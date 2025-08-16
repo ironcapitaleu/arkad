0d32aed9bc95f035f49cc27bc56b5d57b5bdddc0
Demir Catovic <d.catovic9@gmail.com>
2025-08-16 18:41:39 +0200

    feat: add `fmt::Display` for `ChannelConfig`

--- utils/src/queue/shared/channel_config/mod.rs
+++ utils/src/queue/shared/channel_config/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 use crate::queue::shared::channel_type::ChannelType;
 use crate::queue::shared::queue_identifier::QueueIdentifier;
 
@@ -5,4 +7,22 @@ use crate::queue::shared::queue_identifier::QueueIdentifier;
 pub struct ChannelConfig {
     pub channel_type: ChannelType,
     pub queue_identifier: QueueIdentifier,
+}
+
+impl fmt::Display for ChannelConfig {
+    /// Formats the `ChannelConfig` as: "<[`channel_type`](crate::queue::shared::ChannelType)>@<[`queue_identifier`](crate::queue::shared::QueueIdentifier)>"
+    ///
+    /// # Example
+    /// ```
+    /// use utils::queue::shared::{ChannelConfig, ChannelType, QueueIdentifier};
+    ///
+    /// let config = ChannelConfig {
+    ///     channel_type: ChannelType::Producer,
+    ///     queue_identifier: QueueIdentifier::BatchExtractor,
+    /// };
+    /// assert_eq!(format!("{config}"), "Producer@batch_extractor_queue");
+    /// ```
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}@{}", self.channel_type, self.queue_identifier)
+    }
 }
