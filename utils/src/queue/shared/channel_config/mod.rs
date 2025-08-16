4cdbaab686c1be71cf0a1c8e34d88d9157b25c09
Demir Catovic <d.catovic9@gmail.com>
2025-08-16 20:08:46 +0200

    feat: add getters to `ChannelConfig`

--- utils/src/queue/shared/channel_config/mod.rs
+++ utils/src/queue/shared/channel_config/mod.rs
@@ -10,7 +10,7 @@ pub struct ChannelConfig {
 }
 
 impl fmt::Display for ChannelConfig {
-    /// Formats the `ChannelConfig` as: "<[`channel_type`](crate::queue::shared::ChannelType)>@<[`queue_identifier`](crate::queue::shared::QueueIdentifier)>"
+    /// Formats the [`ChannelConfig`] as: "<[`channel_type`](crate::queue::shared::ChannelType)>@<[`queue_identifier`](crate::queue::shared::QueueIdentifier)>"
     ///
     /// # Example
     /// ```
@@ -24,5 +24,41 @@ impl fmt::Display for ChannelConfig {
     /// ```
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}@{}", self.channel_type, self.queue_identifier)
+    }
+}
+
+impl ChannelConfig {
+    /// Returns the [`ChannelType`] for this configuration.
+    ///
+    /// # Example
+    /// ```
+    /// use utils::queue::shared::{ChannelConfig, ChannelType, QueueIdentifier};
+    ///
+    /// let config = ChannelConfig {
+    ///     channel_type: ChannelType::Producer,
+    ///     queue_identifier: QueueIdentifier::BatchExtractor,
+    /// };
+    /// assert_eq!(config.get_channel_type(), ChannelType::Producer);
+    /// ```
+    #[must_use]
+    pub const fn get_channel_type(&self) -> ChannelType {
+        self.channel_type
+    }
+
+    /// Returns the [`QueueIdentifier`] for this configuration.
+    ///
+    /// # Example
+    /// ```
+    /// use utils::queue::shared::{ChannelConfig, ChannelType, QueueIdentifier};
+    ///
+    /// let config = ChannelConfig {
+    ///     channel_type: ChannelType::Producer,
+    ///     queue_identifier: QueueIdentifier::BatchExtractor,
+    /// };
+    /// assert_eq!(config.get_queue_identifier(), QueueIdentifier::BatchExtractor);
+    /// ```
+    #[must_use]
+    pub const fn get_queue_identifier(&self) -> QueueIdentifier {
+        self.queue_identifier
     }
 }
