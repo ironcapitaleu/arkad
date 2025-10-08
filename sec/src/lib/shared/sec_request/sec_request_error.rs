de648621c178219f5c117756de6f4155852149f6
ElecDashi <elecdashi@gmail.com>
2025-10-08 13:23:31 +0200

    feat: add explicit error messages for all errors

--- sec/src/lib/shared/sec_request/sec_request_error.rs
+++ sec/src/lib/shared/sec_request/sec_request_error.rs
@@ -5,7 +5,7 @@ use thiserror::Error;
 ///
 /// This struct provides both the reason for the failure and the user agent string that was provided.
 #[derive(Debug, Error, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-#[error("[SecRequestError] Request failed: Reason: '{reason}'.")]
+#[error("[SecRequestError] Request failed: Reason: `{reason}`.")]
 pub struct SecRequestError {
     /// The reason why the request couldn't be processed.
     pub reason: SecRequestErrorReason,
@@ -25,25 +25,22 @@ impl SecRequestError {
 #[non_exhaustive]
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum SecRequestErrorReason {
-    NetworkError,
+    NetworkError(String),
     HttpError(String),
-    TimeoutError,
+    TimeoutError(String),
     Other(String),
 }
 
 impl From<ReqwestError> for SecRequestError {
     fn from(e: ReqwestError) -> Self {
-        if e.is_timeout() {
-            Self::new(SecRequestErrorReason::TimeoutError)
-        } else if e.is_connect() {
-            Self::new(SecRequestErrorReason::NetworkError)
-        } else if e.is_status() {
-            e.status().map_or_else(
+        match () {
+            () if e.is_timeout() => Self::new(SecRequestErrorReason::TimeoutError(e.to_string())),
+            () if e.is_connect() => Self::new(SecRequestErrorReason::NetworkError(e.to_string())),
+            () if e.is_status() => e.status().map_or_else(
                 || Self::new(SecRequestErrorReason::Other(e.to_string())),
                 |status| Self::new(SecRequestErrorReason::HttpError(status.to_string())),
-            )
-        } else {
-            Self::new(SecRequestErrorReason::Other(e.to_string()))
+            ),
+            () => Self::new(SecRequestErrorReason::Other(e.to_string())),
         }
     }
 }
@@ -52,12 +49,12 @@ impl std::fmt::Display for SecRequestErrorReason {
     /// Formats the reason for display.
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::NetworkError => write!(f, "The HTTP request failed due to a network error."),
+            Self::NetworkError(message) => write!(f, "The HTTP request failed due to a network error: {message}."),
             Self::HttpError(status_code) => write!(
                 f,
                 "The HTTP request failed due to an HTTP error: {status_code}."
             ),
-            Self::TimeoutError => write!(f, "The HTTP request timed out."),
+            Self::TimeoutError(message) => write!(f, "The HTTP request timed out: {message}."),
             Self::Other(message) => write!(
                 f,
                 "The HTTP request failed for an unknown reason: {message}."
