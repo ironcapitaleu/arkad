3e202d64cc859b2fb8b0854e24d831c619b5f972
ElecDashi <elecdashi@gmail.com>
2025-12-24 17:21:31 +0100

    docs: fix invalid linking

--- sec/src/lib/implementations/transitions/from/prepare_sec_request/mod.rs
+++ sec/src/lib/implementations/transitions/from/prepare_sec_request/mod.rs
@@ -1,5 +1,5 @@
 //! # Transitions from `PrepareSecRequest` State
 //!
-//! This module contains transition implementations originating from the [`PrepareSecRequest`](crate::implementations::states::extract::PrepareSecRequest) state.
+//! This module contains transition implementations originating from the [`PrepareSecRequest`](crate::implementations::states::extract::prepare_sec_request::PrepareSecRequest) state.
 
 pub mod execute_sec_request;
--- sec/src/lib/implementations/transitions/from/validate_cik_format/mod.rs
+++ sec/src/lib/implementations/transitions/from/validate_cik_format/mod.rs
@@ -1,5 +1,5 @@
 //! # Transitions from `ValidateCikFormat` State
 //!
-//! This module contains transition implementations originating from the [`ValidateCikFormat`](crate::implementations::states::extract::ValidateCikFormat) state.
+//! This module contains transition implementations originating from the [`ValidateCikFormat`](crate::implementations::states::extract::validate_cik_format::ValidateCikFormat) state.
 
 pub mod prepare_sec_request;
