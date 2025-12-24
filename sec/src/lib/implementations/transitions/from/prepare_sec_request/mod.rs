700bfa27f164629638b37a61bf47c9dba75a2259
ElecDashi <elecdashi@gmail.com>
2025-12-24 17:15:51 +0100

    refactor: add separate transition implementation

---
+++ sec/src/lib/implementations/transitions/from/prepare_sec_request/mod.rs
@@ -0,0 +1,5 @@
+//! # Transitions from `PrepareSecRequest` State
+//!
+//! This module contains transition implementations originating from the [`PrepareSecRequest`](crate::implementations::states::extract::PrepareSecRequest) state.
+
+pub mod execute_sec_request;
---
+++ sec/src/lib/implementations/transitions/from/validate_cik_format/mod.rs
@@ -0,0 +1,5 @@
+//! # Transitions from `ValidateCikFormat` State
+//!
+//! This module contains transition implementations originating from the [`ValidateCikFormat`](crate::implementations::states::extract::ValidateCikFormat) state.
+
+pub mod prepare_sec_request;
