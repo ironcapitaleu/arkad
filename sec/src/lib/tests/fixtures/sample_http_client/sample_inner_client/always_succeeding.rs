6f80969267ec6f10407d140c9578b5cd88786aa6
ElecDashi <elecdashi@gmail.com>
2026-02-27 10:58:37 +0100

    test: add fixture for always succeeding http client

---
+++ sec/src/lib/tests/fixtures/sample_http_client/sample_inner_client/always_succeeding.rs
@@ -0,0 +1,22 @@
+use async_trait::async_trait;
+
+use crate::shared::http_client::traits::InnerClient;
+
+/// A fake HTTP client implementation that always returns a fixed success response.
+/// This client is used for testing the success response handling in the [`HttpClient`] trait implementation by simulating a predefined success scenario.
+#[derive(Debug, Clone)]
+pub struct AlwaysSucceedingHttpClient;
+
+#[async_trait]
+impl InnerClient for AlwaysSucceedingHttpClient {
+    type Request = ();
+    type Response = String;
+    type Error = String;
+
+    async fn execute_request(&self, request: Self::Request) -> Result<Self::Response, Self::Error> {
+        Ok(format!(
+            "Simulated success response for request: {:?}",
+            request
+        ))
+    }
+}
