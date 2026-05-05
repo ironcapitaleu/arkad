0deccafe769d85de8db3e55badb94c8bb5336ee2
Demir Catovic <d.catovic9@gmail.com>
2026-05-05 15:15:50 +0200

    feat: accept both `USD/shares` and `"USD/share` for parsing units inside company data

--- sec/src/lib/shared/financial/unit/mod.rs
+++ sec/src/lib/shared/financial/unit/mod.rs
@@ -55,7 +55,7 @@ impl Unit {
         match s {
             "USD" => Some(Self::Usd),
             "shares" => Some(Self::Shares),
-            "USD/shares" => Some(Self::UsdPerShare),
+            "USD/shares" | "USD/share" => Some(Self::UsdPerShare),
             "pure" => Some(Self::Pure),
             _ => None,
         }
